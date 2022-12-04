6e9a3cba954d7f2e9fd436ec13733fcec7d65d97
vinicius ochner <vinicius.ochner@fulltechpro.com.br>
2022-12-04 15:06:04 -0300

    criando tela de certificados do avaliador

--- lib/view/evaluatorScreen.dart
+++ lib/view/evaluatorScreen.dart
@@ -12,9 +12,65 @@ class ListCertificatesEvaluator extends StatelessWidget {
                   borderRadius: BorderRadius.circular(18.0))));
     }
 
+    Container GetCertificateRegister(){
+      return Container(
+        width: 290,
+        height: 110,
+        margin: EdgeInsets.only(top: 10),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 217,217,217),
+            borderRadius: BorderRadius.circular(10)
+        ),
+        child: Row(
+          children: [
+            Container(
+              width:80,
+              margin: EdgeInsets.all(10),
+              child: Image.asset("assets/images/user_image.png",fit: BoxFit.contain),
+            ),
+            Container(
+                width: 182,
+                child:Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text("Engenharia de software",style:TextStyle(fontSize: 15,fontWeight:FontWeight.bold)),
+                        IconButton(onPressed: ()=>{},icon:Icon(Icons.more_vert))
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.circle,color: Colors.grey,size: 12),
+                        SizedBox(width: 2),
+                        Text("Vinicius Aluno",style:TextStyle(fontSize: 10))
+                      ],
+                    ),
+                    SizedBox(height: 3),
+                    Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          margin: EdgeInsets.all(3),
+                          child: Column(
+                              children: [
+                                Text("Horas:",style: TextStyle(fontSize: 10)),
+                                Text("00h",style: TextStyle(fontSize: 20,fontWeight:FontWeight.bold))
+                              ]
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-        body: Container(
-      child: SingleChildScrollView(
+        body: SingleChildScrollView(
+      child: Container(
         child: Column(children: <Widget>[
           Container(
               decoration: BoxDecoration(
@@ -43,7 +99,7 @@ class ListCertificatesEvaluator extends StatelessWidget {
               ButtonBar(
                 children: [
                   SizedBox(
-                      width: 150,
+                      width: 125,
                       child: ElevatedButton(
                           onPressed: () => {},
                           child: Text("Curso",
@@ -63,7 +119,20 @@ class ListCertificatesEvaluator extends StatelessWidget {
             ],
           ),
           SizedBox(height: 20),
-          Divider(height: 3,color: Color.fromARGB(255, 217, 217, 217),indent: 30,endIndent: 30)
+          Divider(height: 3,color: Color.fromARGB(255, 217, 217, 217),indent: 30,endIndent: 30),
+          Container(
+            child:Column(
+              children: [
+                GetCertificateRegister(),
+                GetCertificateRegister(),
+                GetCertificateRegister(),
+                GetCertificateRegister(),
+                GetCertificateRegister(),
+                GetCertificateRegister(),
+                GetCertificateRegister(),
+              ],
+            )
+          )
         ]),
       ),
     ));
